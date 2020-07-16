c00fad1414ee7b0426f4196a99bc9fe57e62329b
LizvethM <morales-garciaLizveth2021@email.com>
2020-07-16 17:18:35 +0000

    part2_LizvethM

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#PARt1
+####PARt1
 r1 = range(0, 10)
 for range1 in r1:
   print(range1)
@@ -12,6 +12,16 @@ print("")
 r3 = range(10,20)
 for range3 in r3:
   print(range3)
-  print("")
+print("")
 
-  
+####Part2
+
+#variables
+left_boarder = "[!"
+right_boarder = "!]"
+inner = "_"
+name = "Lizveth"
+length = range(0,11)
+
+for length in inner:
+  print(left_boarder+ str(inner)+ name+ str(inner)+ right_boarder)
