c7907979b8ed845dfc3f0493f4f82802ddc0798c
LizvethM <morales-garciaLizveth2021@email.com>
2020-07-16 17:01:41 +0000

    Part1_Lizveth

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 #PARt1
-range[0,10)
+r1 = range(0, 10)
+for range1 in r1:
+  print(range1)
+print("")
+
+r2 = range(5, 30)
+for range2 in r2:
+    print(range2)
+print("")
+
+r3 = range(10,20)
+for range3 in r3:
+  print(range3)
+  print("")
+
+  
